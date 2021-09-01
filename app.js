60491dfb316a5ec45e151d2e596e38257677ee53
Gift-Stack <gkenny896@gmail.com>
2021-09-01 19:11:52 +0100

    .

--- app.js
+++ app.js
@@ -42,6 +42,16 @@ app.use(function (req, res, next) {
 app.use('/auth', require('./routes/auth'))
 app.use('/api', require('./routes/api'))
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) =>
+    res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+}
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
@@ -58,16 +68,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.json({ error: err })
 })
-
-// Serve static assets in production
-
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'))
-
-  app.get('*', (req, res) =>
-    res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'))
-  )
-}
 
 module.exports = { app, sessionStore }
